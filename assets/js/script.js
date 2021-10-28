543d584f71d8e689d8440baed9eecab6b9f3fe27
Mona Lisa <holmesmainsend@gmail.com>
2021-10-28 18:22:46 -0400

    fixed card formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,6 @@ var temp = document.querySelector(".temp");
 var wind = document.querySelector(".wind");
 var humidity = document.querySelector(".humidity");
 var uvIndex = document.querySelector(".uvIndex");
-var cardDate = document.querySelector(".card-date");
-
 
 // Function
 function getWeather(event) {
@@ -50,32 +48,53 @@ function getWeather(event) {
             response2.json().then(function (weatherData) {
               console.log(weatherData);
 
-              // UNIX Conversion
-              var milli = weatherData.current.dt * 1000;
-              var dateObject = new Date(milli);
-              var humanDateTime = dateObject.toLocaleString("en-US", {timezone: "short"})
-              var humanDateFormat = humanDateTime.split(",")[0];
-
-              // Temperature Conversion
-              var fahrenheit = (weatherData.current.temp - 273.15) * (9/5) + 32;
-
               // Page Text Content
               for (i = 0; i < 6; i++) {
                   if (i === 0) {
                     currentBox.classList.add("border", "border-dark");
+
+                    // UNIX Conversion
+                    var milli = weatherData.current.dt * 1000;
+                    var dateObject = new Date(milli);
+                    var humanDateTime = dateObject.toLocaleString("en-US", {timezone: "short"})
+                    var humanDateFormat = humanDateTime.split(",")[0];
+                    
                     cityTitle.textContent = cityName + " (" + humanDateFormat + ")";
+
+                    // Temperature Conversion
+                    var fahrenheit = (weatherData.current.temp - 273.15) * (9/5) + 32;
+                    
                     temp.textContent = "Temp: " + fahrenheit.toFixed(2) + "°F";
                     wind.textContent = "Wind: " + weatherData.current.wind_speed.toFixed(2) + " MPH";
                     humidity.textContent = "Humidity: " + weatherData.current.humidity + "%";
                     uvIndex.textContent = "UV Index: " + weatherData.current.uvi;
+                    
                   } else {
-                      console.log("muffins");
+                      var card = document.querySelector(".card" + [i])
+                      var cardDate = document.querySelector(".card-date" + [i]);
+                      var cardTemp = document.querySelector(".card-temp" + [i]);
+                      var cardWind = document.querySelector(".card-wind" + [i]);
+                      var cardHumidity = document.querySelector(".card-humidity" + [i]);
+                      card.classList.remove("border-0");
+                      card.classList.add("cardcolor");
+
+                        // UNIX Conversion
+                        var milli = weatherData.daily[i].dt * 1000;
+                        var dateObject = new Date(milli);
+                        var humanDateTime = dateObject.toLocaleString("en-US", {timezone: "short"})
+                        var humanDateFormat = humanDateTime.split(",")[0];
+
+                        cardDate.textContent = humanDateFormat;
+
+                        // Temperature Conversion
+                        var fahrenheit = (weatherData.daily[i].temp.day - 273.15) * (9/5) + 32;
+                    
+                        cardTemp.textContent = "Temp: " + fahrenheit.toFixed(2) + "°F";
+                        cardWind.textContent = "Wind: " + weatherData.daily[i].wind_speed.toFixed(2) + " MPH";
+                        cardHumidity.textContent = "Humidity: " + weatherData.daily[i].humidity + "%";
                   }
               }
-              
-              
               input.value = "";
-
             });
           } 
           // Error Handling
@@ -85,7 +104,6 @@ function getWeather(event) {
         });
     }
       });
-      
   });
 };
 }
